bc11effc1688b68ca58a1a94a089f411497e170c
alicesin615 <alicesin0615@gmail.com>
2024-01-24 08:06:23 +0800

    update hardhat.config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,9 +1,12 @@
 import { HardhatUserConfig } from 'hardhat/config';
 import '@nomicfoundation/hardhat-toolbox-viem';
+import '@nomicfoundation/hardhat-ethers';
 import '@nomicfoundation/hardhat-toolbox';
+import '@openzeppelin/hardhat-upgrades';
 
 const config: HardhatUserConfig = {
-    solidity: '0.8.19'
+    solidity: '0.8.20',
+    defaultNetwork: 'hardhat'
 };
 
 export default config;
