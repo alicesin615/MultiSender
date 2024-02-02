f42c12c63938be42ac5dc3cac9877c4b0b2464c4
alicesin615 <alicesin0615@gmail.com>
2024-02-03 07:56:10 +0800

    update import for SafeERC20 to resolve conflict

--- contracts/Multisender.sol
+++ contracts/Multisender.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: MIT
 import {Initializable} from "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
-import "node_modules/@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import "hardhat/console.sol";
 
 pragma solidity ^0.8.20;
