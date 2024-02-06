667525e82d4f2c2665277a91ee071a7af388717f
alicesin615 <alicesin0615@gmail.com>
2024-02-07 06:58:47 +0800

    update hardhat config for testnet deployment

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -4,9 +4,24 @@ import '@nomicfoundation/hardhat-ethers';
 import '@nomicfoundation/hardhat-toolbox';
 import '@openzeppelin/hardhat-upgrades';
 
+const ALCHEMY_API_KEY = process.env.ALCHEMY_API_KEY || '';
+const SEPOLIA_PRIVATE_KEY = process.env.SEPOLIA_PRIVATE_ || '';
+
 const config: HardhatUserConfig = {
-    solidity: '0.8.20',
-    defaultNetwork: 'hardhat'
+    solidity: {
+        compilers: [
+            {
+                version: '0.8.20'
+            }
+        ]
+    },
+    defaultNetwork: 'hardhat',
+    networks: {
+        sepolia: {
+            url: `https://eth-sepolia.g.alchemy.com/v2/${ALCHEMY_API_KEY}`,
+            accounts: [SEPOLIA_PRIVATE_KEY]
+        }
+    }
 };
 
 export default config;
