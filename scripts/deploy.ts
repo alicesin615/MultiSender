abc0519e2381a1da88aab7d3db50ceeb8e209af3
alicesin615 <alicesin0615@gmail.com>
2024-02-07 07:25:23 +0800

    update deploy script

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,26 +1,27 @@
-import { formatEther, parseEther } from "viem";
-import hre from "hardhat";
+import { ethers, upgrades } from 'hardhat';
 
+const initialTotalFunds = process.env.INITIAL_TOTAL_FUNDS || '0';
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const unlockTime = BigInt(currentTimestampInSeconds + 60);
+    const MultisenderContract = await ethers.getContractFactory('Multisender');
+    console.log('Deploying Multisender contract...');
 
-  const lockedAmount = parseEther("0.001");
+    const [sender, ...recipients] = await ethers.getSigners();
 
-  const lock = await hre.viem.deployContract("Lock", [unlockTime], {
-    value: lockedAmount,
-  });
-
-  console.log(
-    `Lock with ${formatEther(
-      lockedAmount
-    )}ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`
-  );
+    const multisenderContract = await upgrades.deployProxy(
+        MultisenderContract,
+        [sender.address, ethers.parseEther(initialTotalFunds)],
+        {
+            initializer: 'initialize'
+        }
+    );
+    await multisenderContract.deployed();
+    console.log(
+        'Multisender contract deployed to:',
+        multisenderContract.address
+    );
 }
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
 main().catch((error) => {
-  console.error(error);
-  process.exitCode = 1;
+    console.error(error);
+    process.exitCode = 1;
 });
